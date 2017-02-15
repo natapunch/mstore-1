0e9b0cf70f7105451eb3260c89998175848ebd5b
ddavidov <ddavidov@mindk.com>
2017-02-15 20:48:31 +0200

    Init

---
+++ src/Page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimmask
+ * Date: 15.02.17
+ * Time: 20:23
+ */
+
+namespace My;
+
+
+class Page
+{
+    public function show(){
+        echo "Hello!";
+    }
+}
